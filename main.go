2ce1429f8d14d6dbc482bdc38d33a5d5c59e586c
Paul <_@u1l.de>
2022-06-05 17:56:09 +0200

    support html

--- main.go
+++ main.go
@@ -9,10 +9,18 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		types := []string{"text/plain"}
+		useragent := r.Header.Get("User-Agent")
+		types := []string{"text/plain", "text/html", "application/json"}
 		switch accept.ServeType(types, r.Header.Get("Accept")) {
 		case "text/plain":
+			w.Header().Add("Content-Type", "text/plain")
 			w.Write([]byte(r.Header.Get("User-Agent")))
+		case "text/html":
+			w.Header().Add("Content-Type", "text/html")
+			w.Write([]byte(`
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+	<h1>What's your user Agent?</h1>
+	<pre>` + useragent))
 		default:
 			http.Error(w, "Could not serve requested Type", http.StatusNotAcceptable)
 		}
